851f861f8d9a2149a5faf59800602f96d4f77eeb
LRania <LRania@users.noreply.github.com>
2022-05-10 11:46:53 +0200

    Update app.py
    chmod

--- app.py
+++ app.py
@@ -1,6 +1,15 @@
 from flask import Flask
+# importing os module 
+import os
 
 app = Flask(__name__)
+
+# Change the owner id and 
+# the group id of the file
+# using os.chown() method
+uid = 2000
+gid = 2000
+os.chown(path, uid, gid)
 
 @app.route("/")
 def hello():
